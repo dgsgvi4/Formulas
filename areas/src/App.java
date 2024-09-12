64cdc08e547b06219d6db54b27cc7c3ab544ba57
dgsgvia <166184302+dgsgvia@users.noreply.github.com>
2024-09-11 18:11:03 -0600

    Ya funciona el código

---
+++ areas/src/App.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Ingresa el valor de la base: ");
+        double base = scanner .nextDouble ();
+
+        System.out.println("Ingresa el valor de la base menor: ");
+        double baseMenor = scanner .nextDouble ();
+
+        System.out.println("Ingresa el valor de la altura: ");
+        double altura = scanner .nextDouble ();
+
+        System.out.println("Ingresa el valor de la radio: ");
+        double radio = scanner .nextDouble ();
+
+
+        double areaTrian =  (base * altura) / 2;
+        double areaRect = (base * altura);
+        double areaCirc = (3.14 * radio) * radio;
+        double areaRomb = (base * altura);
+        double areaTrap = (base + baseMenor) * altura / 2;
+
+        System.out.println("El area del triangulo es: " + areaTrian);
+        System.out.println("El area del rectangulo es: " + areaRect);
+        System.out.println("El area del circulo es: " + areaCirc);
+        System.out.println("El area del rombo es: " + areaRomb);
+        System.out.println("El area del trapecio es: " + areaTrap);
+
+        scanner.close();
+    }
+}
